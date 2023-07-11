58256b831b89a5cba11e2713adacda1143ca71f3
nandala-allan <andala63@gmail.com>
2023-07-11 06:13:41 +0300

    write javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,48 @@
+//  selecting dom elements 
 let taskInput = document.querySelector('input');
 let taskItemList = document.querySelector('#task-list');
+let btn = document.querySelector('button');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// creating a neaw task 
+
+// function createTask() {
+
+
+//     let userInput = taskInput.value.trim();
+
+//     if (userInput.length === 0) {
+//         alert("input cannot be empty");
+//     }
+
+//     let todoListItem = document.createElement('li');
+//     todoListItem.innerText = userInput;
+
+//     return todoListItem;
+
+// }
+
+// function addItem() {
+//     let newTask = createTask();
+//     taskItemList.appendChild(newTask);
+//     taskInput.value = " ";
+// }
+
+
+// btn.addEventListener('click', function () {
+//     addItem();
+// });
